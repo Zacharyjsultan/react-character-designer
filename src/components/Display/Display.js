0fce6caa728a74b418a98d596295a88b570f9ebb
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-09-16 11:20:06 -0700

    working on display

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-export default function Display() {
+export default function Display({ shirtImg, pantImg, shoeImg }) {
   return (
-    <div>Display</div>
+    <div className="display-fit">
+      <div>
+        <img src={`${process.env.Public_URL}/items/${shirtImg}.png`}/>
+
+      </div>
+
+      <div>
+
+      </div>
+
+      <div>
+
+      </div>
+
+    </div>
   );
 }
