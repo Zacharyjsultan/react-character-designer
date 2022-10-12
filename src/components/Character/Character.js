b8c0419741cec82b9a75f9db80037b2b302aa4ec
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-10-12 13:45:34 -0700

    done

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -5,15 +5,15 @@ export default function Display({ shirt, pant, shoe }) {
   return (
     <div className="display-fit">
       <div>
-        <img src={`${process.env.Public_URL}/items/Shirts/${shirt}-shirt.png`} />
+        <img src={`${process.env.PUBLIC_URL}/items/Shirts/${shirt}-shirt.png`} />
       </div>
 
       <div>
-        <img src={`${process.env.Public_URL}/items/Pants/${pant}-pant.png`} />
+        <img src={`${process.env.PUBLIC_URL}/items/Pants/${pant}-pant.png`} />
       </div>
 
       <div>
-        <img src={`${process.env.Public_URL}/items/Shoes/${shoe}-shoe.png`} />
+        <img src={`${process.env.PUBLIC_URL}/items/Shoes/${shoe}-shoe.png`} />
       </div>
     </div>
   );
